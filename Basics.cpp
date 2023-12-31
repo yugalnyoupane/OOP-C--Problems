59101f15377e2f2f61fc3595607cf4192bbba0a0
Yugal Nyoupane <147706135+yugalnyoupane@users.noreply.github.com>
2023-12-31 10:40:12 +0545

    Create Basics.cpp

---
+++ Basics.cpp
@@ -0,0 +1,21 @@
+ #include <iostream>
+ using namespace std;
+
+ int main(){
+    int a,b,c;
+    cout<<"Enter any two numbers:\n";
+    cin>>a>>b;
+    c=a+b;
+    cout<<"\n The addition of two number is: "<<c;
+    c=a-b;
+    cout<<"\n The subtraction of two number is: "<<c;
+    c=a/b;
+    cout<<"\n The division of two number is: "<<c;
+    c=a*b;
+    cout<<"\n The multiplication of two number is: "<<c;
+    return 0;
+
+
+
+
+ }
