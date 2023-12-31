1c4f6e5d7d87e009fe2d22777754c313901f0dcf
Yugal Nyoupane <147706135+yugalnyoupane@users.noreply.github.com>
2023-12-31 10:39:37 +0545

    Create Calculator.cpp

---
+++ Calculator.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+//A simple function to add two numbers
+int add(int a,int b) {
+    return a+b;
+}
+
+class Calculator {
+public:
+    int multiply(int a, int b){
+        return a*b;
+    }
+};
+
+int main(){
+    int x = 5;
+    int y = 3;
+
+    //Using the standalone function 'add'
+    int sum= add(x,y);
+    std::cout<< "Sum: "<< sum << std::endl;
+
+    //Using class and member function
+    Calculator calc;
+    int product = calc.multiply(x,y);
+    std::cout << "Product :"<< product <<std::endl;
+
+    return 0;
+}
